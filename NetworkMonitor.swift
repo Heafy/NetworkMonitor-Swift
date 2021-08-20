e1faf0b4ea57d8cbedf939e6be688db1ef998cb6
Jorge Martinez <jmtz.2878@gmail.com>
2021-08-19 22:54:05 -0500

    NetworkMonitor

---
+++ NetworkMonitor.swift
@@ -0,0 +1,56 @@
+//
+//  NetworkMonitor.swift
+//  TestNetworkMonitor
+//
+//  Created by Jorge Martinez on 15/08/21.
+//
+
+import Network
+import Foundation
+
+@available(macOS 10.14, iOS 12.0, *)
+public final class NetworkMonitor {
+    
+    public enum ConnectionType {
+        case wifi
+        case cellular
+        case ethernet
+    }
+    
+    public static let shared = NetworkMonitor()
+    
+    private let queue = DispatchQueue.global()
+    private let monitor: NWPathMonitor
+    
+    public private(set) var isConnected: Bool = false
+    public private(set) var connectionType: ConnectionType?
+    
+    private init() {
+        self.monitor = NWPathMonitor()
+    }
+    
+    public func startMonitoring() {
+        monitor.start(queue: queue)
+        monitor.pathUpdateHandler = {
+            [weak self] path in
+            self?.isConnected = path.status == .satisfied
+            self?.getConnectionType(path)
+        }
+    }
+    
+    public func stopMonitoring() {
+        monitor.cancel()
+    }
+    
+    private func getConnectionType(_ path: NWPath) {
+        if path.usesInterfaceType(.wifi) {
+            self.connectionType = .wifi
+        } else if path.usesInterfaceType(.cellular) {
+            self.connectionType = .cellular
+        } else if path.usesInterfaceType(.wiredEthernet) {
+            self.connectionType = .ethernet
+        } else {
+            connectionType = nil
+        }
+    }
+}
